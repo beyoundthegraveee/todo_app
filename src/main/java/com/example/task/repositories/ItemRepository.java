fbf0ff267c1049f216484cb4a6fe2fc6465235c3
Kiryl Kurzau <Kiryl.Kurzau@ventionteams.com>
2025-11-18 12:31:43 +0100

    get item by id

--- src/main/java/com/example/task/repositories/ItemRepository.java
+++ src/main/java/com/example/task/repositories/ItemRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends CrudRepository<Item, Integer> {
-
 
 }
